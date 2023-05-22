e7bf0cbd2fd19a60ca602eb763ebb19bcab7fbfd
kgm <gm.kim@softleaf.co.kr>
2023-05-22 15:28:07 +0900

    thymeleaf 기본 설정.

---
+++ src/main/java/com/toyproject/restaurant/config/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.toyproject.restaurant.config;
+
+//@Configuration
+//@EnableWebSecurity
+//public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                    .anyRequest().authenticated()
+//                .and()
+//                    .formLogin()
+//                    .permitAll()
+//                .and()
+//                .logout();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/static/js/**","/static/css/**","/static/img/**","/static/frontend/**");
+//    }
+//}
---
+++ src/main/java/com/toyproject/restaurant/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.toyproject.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MainController {
+    @GetMapping("/home")
+    public String main(HttpServletRequest request) {
+        return "index";
+    }
+}
